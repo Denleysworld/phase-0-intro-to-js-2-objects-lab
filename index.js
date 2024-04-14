2744db9afd270becc5b8172a96f3c25356df3bba
Denleysworld <deekayel05@gmail.com>
2024-04-14 07:25:45 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 const employee = {
-    name: "Denley",
-    streetAddress: "Kileleshwa",
-  };
-  
-  function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return { ...employee, [key]: value };
-  }
-  
-  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-  }
-  
-  function deleteFromEmployeeByKey(employee, key) {
-    const { [key]: omit, ...newObject } = employee;
-    return newObject;
-  }
-  
-  function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
-  }
-  
+  name: "Denley",
+  streetAddress: "Kileleshwa",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return { ...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const { [key]: omit, ...newObject } = employee;
+  return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
